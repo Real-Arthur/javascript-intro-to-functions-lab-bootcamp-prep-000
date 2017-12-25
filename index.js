064510e1ebb97d19319f0904f159f890a711f913
Arthur Carson <carson.bob.arthur@gmail.com>
2017-12-25 07:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  return string.toUpperCase()
-  console.log(`${string}`)
+function logShout('HELLO') {
+  console.log()
 }
